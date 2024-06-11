015eebed3daecd559ef2dc9ed4682de2b8c40321
Stepan Kazakov <103280625+StepanKazakov@users.noreply.github.com>
2024-06-11 22:38:31 +0200

    над каждым логическим методом добавил allure.step

--- api_methods/api_courier.py
+++ api_methods/api_courier.py
@@ -1,3 +1,5 @@
+import allure
+
 from base_methods import BaseMethods
 
 
@@ -5,11 +7,14 @@ class ApiCourier(BaseMethods):
     def __init__(self):
         super().__init__()
 
+    @allure.step('Создание курьера')
     def create_courier(self, payload):
         return self.post("/courier", payload)
 
+    @allure.step('Логин курьера')
     def login_courier(self, payload):
         return self.post("/courier/login", payload)
 
+    @allure.step('Удаление курьера')
     def delete_courier(self, courier_id):
         return self.delete("/courier", object_id=courier_id)
--- api_methods/api_orders.py
+++ api_methods/api_orders.py
@@ -1,3 +1,5 @@
+import allure
+
 from base_methods import BaseMethods
 
 
@@ -5,11 +7,14 @@ class ApiOrders(BaseMethods):
     def __init__(self):
         super().__init__()
 
+    @allure.step('Получения списка всех заказов')
     def get_orders_list(self):
         return self.get_data("/orders")
 
+    @allure.step('Создание заказа')
     def create_order(self, payload):
         return self.post("/orders", payload)
 
+    @allure.step('Получения описания заказа по id')
     def get_order_by_id(self, order_id):
         return self.get_data(f"/orders/track?t={order_id}")
